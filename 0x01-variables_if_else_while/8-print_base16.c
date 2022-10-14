60efebd0285d738b5d8d31aa5090b044faff7989
Mohamed Hedaya <mohamed.hedaya.ss@gmail.com>
2022-10-14 14:53:23 -0700

    8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - entary point
+ * Return: 0
+ */
+int main(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+putchar(i + '0');
+}
+for (i = 97; i < 103; i++)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}

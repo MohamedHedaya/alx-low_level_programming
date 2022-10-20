219e29743a67b4b476943fa241461cc12e59ca08
Mohamed Hedaya <mohamed.hedaya.ss@gmail.com>
2022-10-20 15:03:01 -0700

    0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * isupper - main func
+ * @c: input
+ * Return: 1 if uppercase
+ */
+int isupper(int c)
+{
+if (c >= 65 && c <= 90)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}

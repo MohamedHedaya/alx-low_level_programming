b473e58095ceb83b3b4685ea07898de2e246cb77
Mohamed Hedaya <mohamed.hedaya.ss@gmail.com>
2022-10-28 13:28:51 -0700

    5

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -9,24 +9,18 @@ char *_strncat(char *dest, char *src, int n)
 {
 	int srclen = 0, i = 0;
 	char *temp = dest, *start = src;
-
 	while (*src)
 	{
 		srclen++;
 		src++;
 	}
-
 	while (*dest)
 		dest++;
-
 	if (n > srclen)
 		n = srclen;
-
 	src = start;
-
 	for (; i < n; i++)
 		*dest++ = *src++;
-
 	*dest = '\0';
 	return (temp);
 }

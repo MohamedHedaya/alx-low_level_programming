8d017f5b5fbf95de8aa11dcc5e86ba49af4a35c4
Mohamed Hedaya <mohamed.hedaya.ss@gmail.com>
2022-10-28 13:23:42 -0700

    2

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,7 +11,7 @@ char *_strcat(char *dest, char *src)
 	for (i = 0; dest[i] != '\0'; i++)
 		;
 	for (j = 0; j != '\0'; j++)
-		dest[i+j] = src[j];
+		dest[i + j] = src[j];
 	dest[i + j] = '\0';
 	return dest;
 }

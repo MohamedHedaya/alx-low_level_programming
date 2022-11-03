3737b78e0e85eccb6a71026a62cb9b3a896d7eae
Mohamed Hedaya <mohamed.hedaya.ss@gmail.com>
2022-11-03 10:51:31 -0700

    3

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,13 +6,13 @@
  */
 void _puts_recursion(char *s)
 {
-	if (s[0] == '\0')
+	if (!*s)
 	{
 		_putchar('\n');
 	}
 	else
 	{
-		_putchar(s[0]);
+		_putchar(*s);
 		_puts_recursion(s + 1);
 	}
 	return (0);

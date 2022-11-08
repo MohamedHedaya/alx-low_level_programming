2a0896b5cdcfd8e4b843e7a64619a66b5a1e4631
Mohamed Hedaya <mohamed.hedaya.ss@gmail.com>
2022-11-08 11:34:26 -0800

    12

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,11 +10,12 @@ char *str_concat(char *s1, char *s2)
 {
 	unsigned int size1 = 0, size2 = 0, i = 0;
 	char *result;
-	while (s1[size1] != '\0')
+	
+	while (s1[size1] != '\0' && s1 != NULL)
 		size1++;
-	while (s2[size2] != '\0')
+	while (s2[size2] != '\0' && s2 != NULL)
 		size2++;
-	result = malloc(sizeof (char) * (size1 + size2));
+	result = malloc(sizeof(char) * (size1 + size2));
 	if (result == NULL)
 		return (NULL);
 	while (i < size1)
@@ -27,5 +28,6 @@ char *str_concat(char *s1, char *s2)
 		result[i] = s2[i - size1];
 		i++;
 	}
+	result[i + 1] = '\0';
 	return (result);
 }
